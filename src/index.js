d501390c16ee2e5d64b8dd8f5701b8eb59f15a93
Ilia Okunev <iliaokunev@gmail.com>
2018-09-13 21:44:12 +0300

    sorter

--- src/index.js
+++ src/index.js
@@ -1,31 +1,38 @@
 class Sorter {
-  constructor() {
-    // your implementation
+  
+constructor() {
+  this.vault = [];
+  this.comparator = null;
+}
+add(element) {
+  this.vault.push(element);
+}
+at(index) {
+  return this.vault[index];
+}
+get length() {
+  return this.vault.length;
+}
+toArray() {
+  return this.vault;
+}
+sort(indices) {
+  const newVault = indices.map(k => this.vault
+[k]);
+  indices.sort((a, b) => a - b);
+  if (!this.comparator) {
+   newVault.sort((a, b) => a - b);
+  } else {
+   newVault.sort(this.comparator);
   }
-
-  add(element) {
-    // your implementation
-  }
-
-  at(index) {
-    // your implementation
-  }
-
-  get length() {
-    // your implementation
-  }
-
-  toArray() {
-    // your implementation
-  }
-
-  sort(indices) {
-    // your implementation
-  }
-
-  setComparator(compareFunction) {
-    // your implementation
-  }
+ newVault.forEach((value, k) => {
+    this.vault
+[indices[k]] = value;
+    });
+}
+setComparator(compareFunction) {
+  this.comparator = compareFunction;
+}
 }
 
 module.exports = Sorter;
